7416571c880f623f18bfd80497d92d82641bc646
yasin-yumrutas <yasinyumrutas0@gmail.com>
2024-10-16 23:31:59 +0300

    update

---
+++ IngilizceOgreniyorumExe/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	buildPath := filepath.Join(wd, "my-app", "build")
+	fs := http.FileServer(http.Dir(buildPath))
+	http.Handle("/", fs)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server is running on port %s...\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
