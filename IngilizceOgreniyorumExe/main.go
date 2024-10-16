0b201d58c2969b86738420702fc6a4b9841f9381
yasin-yumrutas <yasinyumrutas0@gmail.com>
2024-10-16 23:26:48 +0300

    update index.html file path

--- IngilizceOgreniyorumExe/main.go
+++ IngilizceOgreniyorumExe/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("my-app/build"))
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	buildPath := filepath.Join(wd, "my-app", "build", "index.html")
+	fs := http.FileServer(http.Dir(buildPath))
 	http.Handle("/", fs)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8081"
+		port = "8080"
 	}
 
 	fmt.Printf("Server is running on port %s...\n", port)
